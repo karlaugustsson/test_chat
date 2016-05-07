5546554224e6eabf792c6a9f3a29ec07f1f4bb28
karl <karl.augustsson@gmail.com>
2016-05-07 14:21:19 +0200

    deleteing everything focusing on nice bootstrap design with a better chat experoience]

--- public/app.js
+++ public/app.js
@@ -28,60 +28,7 @@ server.listen(3000);
 
 io.sockets.on("connect" , function(socket){
 
-	console.log("new user joined the chat")
 
-	socket.on("new_message",function(data,callback){
-		var message = data.message.trim(data.message);
-
-		if(message.substring(0,3) == "/w "){
-			message = message.substring(3,message.length);
-			console.log(message);
-			name_end = message.indexOf(" ");
-			if(name_end != undefined){
-				console.log(name_end + "japps")
-				name = find_user(message.substring(0,name_end));
-				console.log(name);
-				
-				if(name == false){
-					callback("that user was not found try again");
-				}else{
-					message = message.substring(name_end + 1,message.length);
-					console.log(message)
-					message = message.substring(0, name_end +1);
-				}
-
-
-			}
-		}else{
-		data = [{userName:socket.userName , message:message}];
-		io.sockets.emit("update_them_chats",data);			
-		}
-
-	});
-	socket.on("find_user",function(data,callback){
-
-		if(typeof socket.userName === "undefined"){
-		
-		var user  = find_user(data.userName);
-		
-		if(user.length == 0){
-
-			users.push({userName:data.userName , 'socket':socket.id});
-			socket.userName = data.userName;
-
-			socket.emit("start_chat",users);
-			
-			io.sockets.emit("update_user_list" , users);
-
-		}else{
-			callback("user already exists");				
-		}
-
-		}
-		
-
-		
-});
 	
 	socket.on("disconnect",function(){
 		users.splice(users.indexOf(socket.userName));
@@ -90,16 +37,6 @@ io.sockets.on("connect" , function(socket){
 
 	});
 });
-
-
-function find_user(userName){
-	var result = users.filter(function(obj){
-			
-			return obj.userName === userName;
-	});
-
-	return result;
-}
 
 
 
