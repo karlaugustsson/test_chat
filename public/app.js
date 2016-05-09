53cef89bebd59e729500578ca236c3b816255e13
Karl Augustsson <karl.augustsson@gmail.com>
2016-05-09 11:34:47 +0200

    japp

--- public/app.js
+++ public/app.js
@@ -28,15 +28,48 @@ server.listen(3000);
 
 io.sockets.on("connect" , function(socket){
 
-
-	
 	socket.on("disconnect",function(){
 		users.splice(users.indexOf(socket.userName));
 		console.log(socket.userName + " left the chat");
-		io.sockets.emit("update_user_list",users);
+
+		io.sockets.emit("update_users_online_list",users);
 
 	});
+	socket.on("user_exist",function(data,callback){
+	if(users.length != 0){
+
+		if(users.find(function(user){
+	
+			return user.userName.match(/data.userName/gi)}) == null ){
+			
+
+		callback(false);
+
+		create_new_user(socket , data.userName );
+
+		}else{
+			callback(true)
+		}
+
+	}else{
+		create_new_user(socket , data.userName );
+		callback(false);
+	}
+
+	})
+
+	socket.on("send_message",function(data){
+
+		data.userName = socket.userName;
+		io.sockets.emit("update_chat_box",data);
+	});
 });
+
+function create_new_user(socket ,userName){
+	socket.userName = userName;
+	users.push({userName:userName});
+	io.sockets.emit("update_users_online_list",users);
+}
 
 
 
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,16 +1,82 @@
 $( document ).ready(function() {
     
-    var username_form = $("#nameForm");
+    
     var portNumber = 3000;
+
+
     var nameField = $("#nameField");
+    var username_form = $("#nameForm");
+
+
     var socket = io("http://localhost:" + portNumber ).connect();
+    
     var error_box = $("#errorBox");	
+    
+
     var loginForm = $("#loginForm");
     var chatApp = $("#chatApp");
-    var online_users_box = $("#usersOnline");
-    var message_field = $("#messageField");
-    var message_form = $("#messageForm");
-    var message_box = $("#chatBox")
-	
+    var users_online_box = $("#usersOnlineBox");
+    var chat_field = $("#chatField");
+    var message_form = $("#chatForm");
+    var message_box = $("#chatBox");
+    
+    message_box.hide();
+    message_form.hide();
+    users_online_box.hide();
+    error_box.hide();
+
+
+    username_form.on("submit",function(e){
+
+    	var username = nameField.val();
+
+    	e.preventDefault();
+    	socket.emit("user_exist",{userName:username},function(answer){
+    		
+    		if(answer == false){
+    			chat_field.val("");
+    			start_chat(username);
+    		}else{
+    			error_box.show();
+    			console.log(answer);
+    			error_box.html("sorry bro that username is already taken");
+    		}
+    	})
+    });
+    message_form.on("submit" , function(e){
+    	e.preventDefault();
+    	socket.emit("send_message",{message:chat_field.val()});
+    	chat_field.val("");
+    })
+    socket.on("update_users_online_list",function(users){
+   
+    	users_online_box.html("");
+   
+    	users.map(function(user){
+    		users_online_box.append('<span class="user">' + user.userName + '</span>');
+    	});	
+
+    });
+    socket.on("update_chat_box",function(data){
+ 
+    		message_box.append( "<p>" + data.userName + ":" + data.message + "</p>");
+    });
+
+    function start_chat(){
+    username_form.hide();
+    error_box.hide();
+
+    message_box.show();
+    message_form.show();
+    users_online_box.show();
+   
+    }
+	socket.on('disconnect',function(){
+
+   		alert("connection lost the page will reload once dialog closed");
+   		location.reload();
+
+
+	});
 
 });
